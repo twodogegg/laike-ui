c5f98d0549a718d1c311e0256d25bd8aebacf5a3
shuliuzhenhua <971270272@qq.com>
2020-04-23 10:34:13 +0800

    添加富文本组件

---
+++ src/components/rich-text/rich-text.vue
@@ -0,0 +1,34 @@
+<!--uni-app框架下 富文本解析-->
+<template>
+  <view class="rich-text-wrap">
+    <rich-text class="rich-text" :nodes="nodes" />
+  </view>
+</template>
+
+<script>
+  import htmlParse from '../../common/html-parser'
+  export default {
+    props: {
+      content: {
+        type: String,
+        default: '',
+      }
+    },
+    computed: {
+      nodes() {
+        let content = this.content.replace(/src/g,"style='width:100%!important;height: auto!important;' src").replace(/table/g,`table style="width:100%!important;"`)
+        return htmlParse(content)
+      }
+    },
+  }
+</script>
+
+<style scoped lang="less">
+  .rich-text-wrap {
+    width: 100%;
+    max-width: 100%;
+    .rich-text {
+      font-size: 14px;width: 100%;max-width: 100%;
+    }
+  }
+</style>
